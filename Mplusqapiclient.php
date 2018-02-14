edb76e23c2db3c947df9c3f2577fdc9495af8b83
Barry vd. Heuvel <barryvdh@gmail.com>
2018-02-14 11:56:15 +0100

    Parse DateTime object properly

--- Mplusqapiclient.php
+++ Mplusqapiclient.php
@@ -4973,6 +4973,11 @@ class MplusQAPIDataParser
 
   public function convertMplusDate($timestamp, $field_name)
   {
+    // Convert DateTime object to timestamp
+    if ($timestamp instanceof \DateTime) {
+      $timestamp = $timestamp->getTimestamp();
+    }
+    
     if (is_array($timestamp)) {
       // Probably already properly converted
       return $timestamp;
@@ -4996,6 +5001,11 @@ class MplusQAPIDataParser
 
   public function convertMplusDateTime($timestamp, $field_name)
   {
+    // Convert DateTime object to timestamp
+    if ($timestamp instanceof \DateTime) {
+      $timestamp = $timestamp->getTimestamp();
+    }
+    
     if (is_array($timestamp)) {
       // Probably already properly converted
       return $timestamp;
